ec0200f12d3015e8498a72baff540a958be0daf4
KovtunovIvan <kovtunovtn@gmail.com>
2024-11-03 22:24:51 +0500

    Start project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Button from './components/Button/Button.jsx';
+import JournalItem from './components/JournalItem/JournalItem.jsx';
+
+function App() {
+
+  return (
+      <>
+          Проект
+          <Button />
+          <JournalItem />
+      </>
+  );
+}
+
+export default App;
